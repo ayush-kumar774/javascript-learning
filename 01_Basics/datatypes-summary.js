87d47177a0e6187aaaa4c6d715cbcabcbe192698
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2023-07-12 01:29:58 +0530

    Stack and heap memory

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -1,4 +1,4 @@
-// Primitive (Call by value)
+// Primitive (Call by value) are stored in Stack.
 /*
 1. String
 2. Number
@@ -25,13 +25,13 @@ const id = Symbol('123');
 const anotherId = Symbol('123');
 
 // console.log(id === anotherId);
-console.log(typeof id);
+// console.log(typeof id);
 
 
 const bigNumber = 683628964836n;
 // console.log(`bigNumber: ${typeof bigNumber}, Value:${bigNumber}`);
 
-// Non-primitive (Reference type)
+// Non-primitive (Reference type) are stored in HEAP.
 /*
 1. Arrays
 2. Objects
@@ -56,3 +56,27 @@ const myFunction = function greet() {
 // console.table(myObj);
 // myFunction();
 // console.log(typeof myFunction);
+
+
+// Stack(Primitive) 
+
+let petName = "Rocky";
+let anotherPetName = petName;
+anotherPetName = "Raja";
+
+console.log(`Petname = ${petName} and Another Petname = ${anotherPetName}`) ;
+
+// Heap(Non primitive)
+
+let userOne =  {
+    firstName : "John",
+    lastName : "Doe",
+    emailId : "johndoe@gmail.com"
+}
+
+let userTwo = userOne;
+
+userTwo.emailId = "johndoe@yahoomail.com";
+
+console.log(`User one email id ${userOne.emailId}
+User two email id  ${userTwo.emailId}`); // Both the values will
