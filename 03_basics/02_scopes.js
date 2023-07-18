9fd077defaf5d63adc2ce2978a6a3c4c7522fc76
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2023-07-19 00:55:48 +0530

    scopes in javascript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,13 +1,49 @@
 let a = 300;
 
-if (true) {
-    let a = 10; // it will give the error message if accessed outside of the scope.
-    const b = 20;
-    var c = 30; // it is globally declared.
-    console.log("INNER A : ", a);
-};
+// if (true) {
+//     let a = 10; // it will give the error message if accessed outside of the scope.
+//     const b = 20;
+//     var c = 30; // it is globally declared.
+//     console.log("INNER A : ", a);
+// };
 
 
-console.log("OUTER A : ", a); 
+// console.log("OUTER A : ", a); 
 // console.log(b); 
 // console.log(c);
+
+function one() {
+    const username = "Aayush" ;
+
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+
+    // console.log(website);
+    two();
+}
+
+// one();
+
+if (true) {
+    const username = "Aayush";
+    if (username === "Aayush") {
+        const website = " youtube";
+        console.log(username + website);
+    }
+}
+
+// console.log(addOne(5)); // it will work 
+function addOne(num) {
+    return num + 1;
+}
+
+console.log(addOne(5));
+
+
+// console.log(addTwo(5)); // Cannot access 'addTwo' before intitalization because we are holding it in variable.
+const addTwo = function (num) {
+    return num + 2 ;
+}
+console.log(addTwo(5));
