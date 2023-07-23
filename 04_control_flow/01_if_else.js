3cbe4dc1a664f154b597f201d710db65f871926e
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2023-07-23 12:26:07 +0530

    control flows in javascript

---
+++ 04_control_flow/01_if_else.js
@@ -0,0 +1,47 @@
+// if else statement
+
+// const isUserLoggedIn = true;
+// const temperature = 41 ;
+
+// if (isUserLoggedIn) {
+//     console.log("Welcome back!");
+// }
+
+// if (temperature < 50) {
+//     console.log("Less than 50");
+// }
+// else {
+//     console.log("Temperature is greater than 50");
+// }
+
+// const score = 200 ;
+
+// if (score > 100) {
+//     const power = "Flying";
+//     console.log(`User power is : ${power}`);
+// }
+
+// const balance = 1000 ;
+// if (balance > 500) console.log("Test"), console.log("Test2"); // Not a good practice
+// if (balance < 500) {
+//     console.log("Less than 500");
+// } 
+// else if (balance == 500) {
+//     console.log("Equal to 500");
+// }
+// else {
+//     console.log("Greater than 500");
+// }
+
+// const userLoggedIn = true;
+// const debitCard = true;
+// const loggedInFromGoogle = false;
+// const loggedInFromEmail = true;
+
+// if (userLoggedIn && debitCard) {
+//     console.log("Allow to buy course");
+// }
+
+// if (loggedInFromEmail || loggedInFromGoogle) {
+//     console.log("User logged in");
+// }
---
+++ 04_control_flow/03_truthy_falsy.js
@@ -0,0 +1,19 @@
+const userEmail = "aksp8134@gmail.com";
+
+if (userEmail) {
+    console.log("Got user email");
+}
+else {
+    console.log("Don't have user email");
+}
+
+// Falsy Values
+// false, 0, -0, BingInt 0n, "", null, undefined, NaN
+
+// Truthy Values
+// "0", 'false', " ", [], {}, function() {}
+
+const emptyObj = {} ;
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Empty object");
+}
