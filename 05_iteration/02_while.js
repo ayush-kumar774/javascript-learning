5f01d3f2dff1c8722a9889f20a3977082e8db08a
ayush-kumar774 <ayushkumar1234@protonmail.com>
2023-07-24 23:50:52 +0530

    while and do-while loop in javascript

---
+++ 05_iteration/02_while.js
@@ -0,0 +1,20 @@
+// let index = 1 ;
+// while (index <= 10) {
+//     console.log(`Value of index = ${index}`);
+//     index++;
+// } 
+
+// let myArray = ['Flash', "Batman", "Superman", "Aquaman", "Cybrog"];
+
+// let arrIndex = 0 ;
+
+// while(arrIndex < myArray.length) {
+//     console.log(`Value is = ${myArray[arrIndex]}`);
+//     arrIndex++;
+// }
+
+let index = 11 ;
+do {
+    console.log(index);
+    index++;
+} while(index <= 10);
