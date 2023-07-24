7aefa32abefcdbd336f797ca09c6f109e0f5cbd6
ayush-kumar774 <ayushkumar1234@protonmail.com>
2023-07-24 22:46:14 +0530

    for loops in javascript

---
+++ 05_iteration/01_for_loops.js
@@ -0,0 +1,7 @@
+// for loops
+
+const array = [1, 2, 3, 4, 5];
+for (let index = 0; index < array.length; index++) {
+    console.log(`Elements of the array = ${array[index]}`);
+}
+// console.log(array);
