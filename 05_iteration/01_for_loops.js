70bcec8c03df04f9ce74b142760f44e27bb61b56
ayush-kumar774 <ayushkumar1234@protonmail.com>
2023-07-24 23:19:15 +0530

    for loops in javascript

--- 05_iteration/01_for_loops.js
+++ 05_iteration/01_for_loops.js
@@ -1,7 +1,37 @@
 // for loops
 
-const array = [1, 2, 3, 4, 5];
-for (let index = 0; index < array.length; index++) {
-    console.log(`Elements of the array = ${array[index]}`);
-}
+// const array = [1, 2, 3, 4, 5];
+// for (let index = 0; index < array.length; index++) {
+//     // console.log(index);
+//     console.log(`Elements of the array = ${array[index]}`);
+// }
 // console.log(array);
+
+
+// for (let i = 1; i <= 20 ; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j + ' = ' + (i *j));
+//     }
+// }
+
+// break and continue
+
+// BREAK
+// for (let index = 1 ; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break;
+//     }
+//     console.log(`Value of i is ${index}`);
+// }
+
+// CONTINUE
+// for (let index = 1 ; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue;
+//     }
+//     console.log(`Value of i is ${index}`);
+// }
